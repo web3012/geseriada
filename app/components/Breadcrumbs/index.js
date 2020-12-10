692e6e970feeab64d93c8f923f430027aa8cf7c9
bumka <web3012@mail.ru>
2020-12-10 14:49:55 +0800

    tuning

--- app/components/Breadcrumbs/index.js
+++ app/components/Breadcrumbs/index.js
@@ -12,9 +12,9 @@ const Breadcrumbs = (props) => {
         {path.length > 0 && <div className={`breadcrumbs ${styles.wr}`}>
             <ul>
                 <li><Link href="/"><a>Главная</a></Link></li>
-                {path.map(el=>{
+                {path.map((el,i)=>{
                     return (
-                        <React.Fragment>
+                        <React.Fragment key={i}>
                             {el.url && <li><Link href={el.url}><a>{el.title}</a></Link></li>}
                             {!el.url && <li><span>{el.title}</span></li>}
                         </React.Fragment>
