116deafe1e8d92a4192d37a7dd7a97a7d66934c7
bumka <web3012@mail.ru>
2020-12-10 14:11:34 +0800

    add w240

--- pages/author/[slug].js
+++ pages/author/[slug].js
@@ -6,54 +6,57 @@ import Link from "next/link"
 const PageAuthor = (props) => {
 
     const router = useRouter()
-    console.log("author", props.author)
     let author = props.author || {}
 
     return (
-        <Layout breadcrumbs={[{url:"/catalog", title:"Каталог"}, {title:author.fio}]}>
+        <Layout breadcrumbs={[{ url: "/catalog", title: "Каталог" }, { title: author.fio }]}>
             <div className="wr">
                 <div className="content">
                     <div className="txt">
-                        
-
                         <div className="page-author">
+
+                            <div className="title"><h1>{author.fio}</h1></div>
+
                             <div className="foto">
                                 <div className="pic">
-                                    <img src={author.foto450}/>
+                                    <img src={author.foto450} />
                                 </div>
+                            </div>
+
+                            <div className="bio">
                                 <div className="meta">
                                     <p>{author.meta.год}</p>
                                     <p>{author.meta.титул}</p>
                                 </div>
 
-                            </div>
-                            <div className="bio">
-                                <h1>{author.fio}</h1>
                                 <div dangerouslySetInnerHTML={{ __html: author.content }} />
                             </div>
+
                             <div className="pics">
-                                <hr/>
                                 <div className="items">
-                                {author.pictures && author.pictures.map((el,i)=>{
-                                    return (
-                                        <div key={i} className="item">
-                                            <div className="item-img"><img src={`/_data/w120/${el._img}.jpg`} width="120" title={el.meta.название}/></div>
-                                            <div className="item-txt">
-                                                <p>Название: <Link href={`/author/${el.author.dir}/${el._img}`}><a>{el.meta.название}</a></Link></p>
-                                                <p>Год: {el.meta.год}</p>
-                                                <p>Инв. N: Г-{el.meta.код}</p>
-                                                <p>Подписи на изображении: {el.meta['подписи на изображении']}</p>
-                                                <p>Поступление: {el.meta.поступление}</p>
-                                                <p>Размер: {el.meta.размер}</p>
-                                                <p>Техника: {el.meta.техника}</p>
+                                    {author.pictures && author.pictures.map((el, i) => {
+                                        return (
+                                            <div key={i} className="item">
+                                                <div className="item-img">
+                                                    <img src={`/_data/w240/${el._img}.png`} width="240" title={el.meta.название} />
+                                                </div>
+                                                <div className="item-txt">
+                                                    <p>Название: <Link href={`/author/${el.author.dir}/${el._img}`}><a>{el.meta.название}</a></Link></p>
+                                                    <p>Год: {el.meta.год}</p>
+                                                    <p>Инв. N: Г-{el.meta.код}</p>
+                                                    <p>Подписи на изображении: {el.meta['подписи на изображении']}</p>
+                                                    <p>Поступление: {el.meta.поступление}</p>
+                                                    <p>Размер: {el.meta.размер}</p>
+                                                    <p>Техника: {el.meta.техника}</p>
+                                                </div>
                                             </div>
-                                        </div>
-                                    )
-                                })}
+                                        )
+                                    })}
                                 </div>
                             </div>
-                        </div>
 
+
+                        </div>
                     </div>
                 </div>
             </div>
